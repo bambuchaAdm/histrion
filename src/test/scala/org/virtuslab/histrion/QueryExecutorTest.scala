868d543f5786e287606daa57bef1a387f5b176ed
Łukasz Dubiel <bambucha14@gmail.com>
2014-02-01 07:55:09 +0100

    Add unit test for executor.

--- src/test/scala/org/virtuslab/histrion/QueryExecutorTest.scala
+++ src/test/scala/org/virtuslab/histrion/QueryExecutorTest.scala
@@ -4,6 +4,7 @@ import org.scalatest.{FlatSpec, Matchers}
 import org.scalatest.mock.MockitoSugar
 import scala.slick.driver.H2Driver.simple._
 import scala.concurrent.ExecutionContext
+import org.mockito.Mockito
 
 /**
  * Created by bambucha on 01.02.14.
@@ -12,8 +13,16 @@ class QueryExecutorTest extends FlatSpec with Matchers with MockitoSugar {
 
   behavior of classOf[QueryExecutor].getSimpleName
 
+  import Mockito._
+  import org.mockito.Matchers._
+
   it should "call execution context on scheduling query" in {
-    val queryExecutor = new QueryExecutor(mock[Database],mock[ExecutionContext])
+    val executionContext = mock[ExecutionContext]
+    val query = mock[Query[_, _]]
+    val queryExecutor = new QueryExecutor(mock[Database], executionContext)
+    queryExecutor.scheduleSelect(query)
+    verify(executionContext).execute(isA(classOf[SelectFuture[_, _]]))
+
   }
 
 }
