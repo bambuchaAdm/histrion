6aefe2e1453b70f3fc5f50803db90fa82e8cb8c9
Łukasz Dubiel <bambucha14@gmail.com>
2014-02-01 15:50:40 +0100

    Move mocks to local tests.

--- src/test/scala/org/virtuslab/histrion/QueryExecutorTest.scala
+++ src/test/scala/org/virtuslab/histrion/QueryExecutorTest.scala
@@ -17,27 +17,28 @@ class QueryExecutorTest extends FlatSpec with Matchers with MockitoSugar {
   import org.mockito.Matchers._
 
   val executionContext = mock[ExecutionContext]
-  val query = mock[Query[Table[_], _]]
-  val updateQuery = mock[Query[Column[Int], Int]]
   val database = mock[Database]
   val queryExecutor = new QueryExecutor(database, executionContext)
 
   override protected def withFixture(test: NoArgTest): Outcome = {
-    reset(executionContext, query, database, updateQuery)
+    reset(executionContext, database)
     test()
   }
 
   it should "schedule select future on  select query" in {
+    val query = mock[Query[Table[_], _]]
     queryExecutor.scheduleSelect(query)
     verify(executionContext).execute(isA(classOf[SelectFuture[_, _]]))
   }
 
   it should "schedule delete future on delete query" in {
+    val query = mock[Query[Table[_], _]]
     queryExecutor.scheduleDelete(query)
     verify(executionContext).execute(isA(classOf[DeleteFuture[_, _]]))
   }
 
   it should "schedule update future on update query" in {
+    val updateQuery = mock[Query[Column[Int], Int]]
     queryExecutor.scheduleUpdate(updateQuery,5)
     verify(executionContext).execute(isA(classOf[UpdateFuture[_, _]]))
   }
